a1d9eed0b53b70d6c1ee7dba1f5bd1ddbbc37006
Orion Forowycz <oforowycz@gmail.com>
2024-09-26 16:00:48 +0200

    better mobile

--- js/script.js
+++ js/script.js
@@ -4,20 +4,6 @@ AOS.init({
     once: true,
 });
 
-// Smooth Scrolling for Internal Links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if(target){
-            target.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        }
-    });
-});
-
 // Mobile Navigation Toggle
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
