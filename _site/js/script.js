8f3505ffbd10064f1c17b850e40822e25b033bf5
Orion Forowycz <oforowycz@gmail.com>
2024-09-26 15:55:30 +0200

    jekyll site

---
+++ _site/js/script.js
@@ -0,0 +1,27 @@
+// Initialize AOS Library
+AOS.init({
+    duration: 1000,
+    once: true,
+});
+
+// Smooth Scrolling for Internal Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if(target){
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+// Mobile Navigation Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+});
